86af973c42d3a47111d5021fd388aef8565490d9
sandhya412cs <sandhya412@gmail.com>
2026-09-16 16:10:56 +0530

    my third commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
- public class Main {
+ public class Main{
 
         private static WebDriver driver;
 
@@ -30,4 +30,4 @@ import org.testng.annotations.Test;
             driver.quit();
         }
     }
-}
+
