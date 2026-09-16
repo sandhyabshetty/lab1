e9b40514174276f18654b7884180251f1a54b3d0
sandhya412cs <sandhya412@gmail.com>
2026-09-16 16:06:20 +0530

    my second commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+ public class Main {
+
+        private static WebDriver driver;
+
+        @BeforeTest
+        public void openBrowser() throws InterruptedException {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            Thread.sleep(2000);
+            driver.get("https://sandhyabshetty.github.io/lab1/");
+        }
+
+        @Test
+        public void titleValidationTest() {
+            String actualTitle = driver.getTitle();
+            String expectedTitle = "Tripillar Solutions";
+            Assert.assertEquals(actualTitle, expectedTitle);
+            Assert.assertTrue(true, "Title should contain 'Tripillar'");
+        }
+
+        @AfterTest
+        public void closeBrowser() throws InterruptedException {
+            Thread.sleep(1000);
+            driver.quit();
+        }
+    }
+}
